f2e126e07ae4d4c82ad73de0d9e00ddcda059a7c
Sandile Ubuntu <sandile@adclickafrica.com>
2024-09-03 16:23:46 +0200

    testing logs 2

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('/','FundingController');
 
-Route::get('/', [FundingController::class, 'index']);
+Route::post('/', [FundingController::class, 'index']);
